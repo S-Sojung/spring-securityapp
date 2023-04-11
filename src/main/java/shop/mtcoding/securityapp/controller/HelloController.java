6eb412e15ccbfc6a12d51e3c0a03c560f3feedc4
sojung <sojung528@gmail.com>
2023-04-11 09:55:34 +0900

    AuthenticationPrincipal 어노테이션 사용

--- src/main/java/shop/mtcoding/securityapp/controller/HelloController.java
+++ src/main/java/shop/mtcoding/securityapp/controller/HelloController.java
@@ -1,11 +1,14 @@
 package shop.mtcoding.securityapp.controller;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import lombok.RequiredArgsConstructor;
+import shop.mtcoding.securityapp.core.auth.MyUserDetails;
 import shop.mtcoding.securityapp.dto.ResponseDTO;
 import shop.mtcoding.securityapp.dto.UserRequest;
 import shop.mtcoding.securityapp.dto.UserResponse;
@@ -15,6 +18,15 @@ import shop.mtcoding.securityapp.service.UserService;
 @Controller
 public class HelloController {
     private final UserService userService;
+
+    @GetMapping("/users/{id}")
+    public ResponseEntity<?> userCheck(@PathVariable Long id, @AuthenticationPrincipal MyUserDetails myUserDetails) {
+        // AuthenticationPrincipal는 Authentication에 있는 pricipal 자리에 들어간다.
+        // 이 자리에는 user 및 role이 있다.
+        String username = myUserDetails.getUser().getUsername();
+        String role = myUserDetails.getUser().getRole();
+        return ResponseEntity.ok().body(username + " : " + role);
+    }
 
     @GetMapping("/")
     public ResponseEntity<?> hello() {
